16e0c33fc825189e84dec8148e97b4d05c4a9f32
vinayak patil <vinayakpatil5310@gmail.com>
2026-01-25 11:11:28 +0530

    Even Number

--- Core_basic/EvenNo.java
+++ Core_basic/EvenNo.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class EvenNo {
     public static void main(String[] args) {
+        int n;
         Scanner obj = new Scanner(System.in);
         System.out.println("Enetr a range: ");
-        int n = obj.nextInt(); 
+        n = obj.nextInt(); 
         for(int i=1; i<=n; i++){
             if (i % 2 == 0) {
                 System.out.println(i);
