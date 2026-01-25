bec623286fd7a5b09db513ce4619c377a37ec0a5
vinayak patil <vinayakpatil5310@gmail.com>
2026-01-25 11:15:04 +0530

    Average of numbers

--- Core_basic/Average.java
+++ Core_basic/Average.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
+        int a;
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Marks of 4 Subjects: ");
-        int a = sc.nextInt();
+        a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
         int d = sc.nextInt();
