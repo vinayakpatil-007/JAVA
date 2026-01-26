4ac886eb1bc9e80806250b3d22be196e6d2accd7
vinayak patil <vinayakpatil5310@gmail.com>
2026-01-26 20:00:23 +0530

    If else codition

--- Core_Basic_Java_Problems/If_else.java
+++ Core_Basic_Java_Problems/If_else.java
@@ -4,5 +4,15 @@ public class If_else {
     public static void main(String[] args) {
         Scanner n = new Scanner(System.in);
         System.out.println("Enter age :");
+        int age = n.nextInt();
+
+        if (age>18) {
+            System.out.println("you are Eligible for vote");
+        }
+
+        else{
+            System.out.println("you are not Eligible for vote");
+        }
+
     }
 }
