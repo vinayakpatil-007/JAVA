015c5886881cc4dc430f6b3edf30ef45a8e9407e
vinayak patil <vinayakpatil5310@gmail.com>
2026-01-26 20:03:05 +0530

    updated folder

--- Core_Basic_Java_Problems/Simple_if.java
+++ Core_Basic_Java_Problems/Simple_if.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Simple_if {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        System.out.println("Entter age :");
+        System.out.println("Enter age :");
         int age = obj.nextInt();
 
         if (age>=18) {
-            System.out.println("Eligibe for voite ");
+            System.out.println("Eligible for vote ");
         }
     }
 }
