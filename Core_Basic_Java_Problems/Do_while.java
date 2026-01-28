ced9b03a8912f66f0d9fcc8da730f12c5c289cd7
vinayak patil <vinayakpatil5310@gmail.com>
2026-01-28 15:04:05 +0530

    do while loop

---
+++ Core_Basic_Java_Problems/Do_while.java
@@ -0,0 +1,13 @@
+public class Do_while {
+    public static void main(String[] args) {
+        int a=1;
+
+        do{
+            System.out.println("count =" +a);
+            ++a;
+
+        }
+
+        while(a<=10);
+    }
+}
