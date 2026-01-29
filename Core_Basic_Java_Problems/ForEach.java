f2bec5872b3b816a529b590d5fe1979bdb722991
vinayak patil <vinayakpatil5310@gmail.com>
2026-01-29 18:31:49 +0530

    For Each loop

---
+++ Core_Basic_Java_Problems/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int a[] = {10,20,30,40,50,60,70};
+
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
+}
