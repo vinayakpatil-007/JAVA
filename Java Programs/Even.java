6bc1f75914aba8a0a1d480696a59fa19dc69686f
vinayak patil <vinayakpatil5310@gmail.com>
2026-02-01 19:16:36 +0530

    Even Number

--- Java Programs/Even.java
+++ Java Programs/Even.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Even6 {
+public class Even {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter a Number  :");
