94ccd74bec02f678deb9bea0c36a356310db881b
vinayak patil <vinayakpatil5310@gmail.com>
2026-02-04 18:32:37 +0530

    Swapping of Two Number

---
+++ Java programs01.java/Swapping.java
@@ -0,0 +1,13 @@
+public class Swapping {
+    public static void main(String[] args) {
+        int a=10,b=20,t;
+        System.out.println("a ="+a+" ,b ="+b);
+
+        t=a;
+        a=b;
+        b=t;
+
+        System.out.println("a ="+a+" ,b ="+b);
+
+    }
+}
