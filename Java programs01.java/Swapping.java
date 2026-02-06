7cc699c6fecd11408dc270d640a7a2c537927bd2
vinayak patil <vinayakpatil5310@gmail.com>
2026-02-06 19:18:42 +0530

    Swpping a two number

--- Java programs01.java/Swapping.java
+++ Java programs01.java/Swapping.java
@@ -11,3 +11,19 @@ public class Swapping {
 
     }
 }
+
+// Swapping without using third variable
+
+// public class Swapping {
+//     public static void main(String[] args) {
+//         int a=10,b=20,t;
+//         System.out.println("a ="+a+" ,b ="+b);
+
+//         a=a+b;
+//         b=a-b;
+//         a=a-b;
+
+//         System.out.println("a ="+a+" ,b ="+b);
+//     }
+
+// }
