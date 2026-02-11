29c82ccdfe862bc3eb329d6487260077febe275e
vinayak patil <vinayakpatil5310@gmail.com>
2026-02-11 19:12:19 +0530

    Simple Array

---
+++ Arrays in Java/Simple_Array.java
@@ -0,0 +1,14 @@
+
+
+public class Simple_Array {
+    public static void main(String[] args) {
+        int a[] = new int[4]; 
+        
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+        a[3] = 40;
+
+        System.out.println(a[2]);
+    }
+}
