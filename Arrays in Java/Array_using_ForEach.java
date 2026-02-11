7d8bad859d79bb5a2d01490817a31eb8aa13b8ee
vinayak patil <vinayakpatil5310@gmail.com>
2026-02-11 17:59:51 +0530

    Array using ForEach

---
+++ Arrays in Java/Array_using_ForEach.java
@@ -0,0 +1,10 @@
+
+public class Array_using_ForEach {
+    public static void main(String[] args) {
+        int a[]={2,4,6,8,10};
+
+        for (int i : a) {
+            System.out.print(i+" ");
+        }
+    }
+}
