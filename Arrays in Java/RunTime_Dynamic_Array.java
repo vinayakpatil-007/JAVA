7f9381c82a17a965bf7bab585666188feb81ae04
vinayak patil <vinayakpatil5310@gmail.com>
2026-02-11 19:10:18 +0530

    RunTime Dynamic Array

---
+++ Arrays in Java/RunTime_Dynamic_Array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RunTime_Dynamic_Array {
+    public static void main(String[] args) {
+        int size;
+
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter Size of Array : ");
+        size =obj.nextInt();
+        int a[] = new int[size];
+
+        System.out.println("Enter Array Element :");
+
+        for (int i = 0; i < size; i++) {
+            a[i] = obj.nextInt();
+        }
+
+        System.out.println("Array Elements :");
+        for (int i = 0; i < size; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
