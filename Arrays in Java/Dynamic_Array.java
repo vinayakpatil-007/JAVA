a441569a82ea974b8f9bb66a88b4b13a43d5bcd3
vinayak patil <vinayakpatil5310@gmail.com>
2026-02-11 18:22:26 +0530

    Dynami Array

---
+++ Arrays in Java/Dynamic_Array.java
@@ -0,0 +1,10 @@
+
+public class Dynamic_Array {
+    public static void main(String[] args) {
+        int a[]={10,20,30,40,50};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
