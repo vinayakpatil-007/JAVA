ae9ec0ae5371be88197ab1501144cae9734c5267
vinayak patil <vinayakpatil5310@gmail.com>
2026-02-13 22:51:11 +0530

    ArraysClass toString,asList,deepToString

---
+++ Arrays in Java/ArraysClass_Methods01.java
@@ -0,0 +1,14 @@
+
+import java.util.Arrays;
+
+public class ArraysClass_Methods01 {
+    public static void main(String[] args) {
+        String a[]={"vin","taa","pc","pp"};
+
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.asList(a));
+
+        int arr[][]={{1,2},{3,4}};
+        System.out.println(Arrays.deepToString(arr));
+    }
+}
