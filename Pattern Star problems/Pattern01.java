daae296aec3727cd200799557855c8b44e19d911
vinayak patil <vinayakpatil5310@gmail.com>
2026-02-18 17:08:10 +0530

    Simple Pattern

--- Pattern Star problems/Pattern01.java
+++ Pattern Star problems/Pattern01.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Pattern01 {
     public static void main(String[] args) {
-        
+        Scanner o =new Scanner(System.in);
+        System.out.println("Enter number of Rows And Colomn :");
+        int a =o.nextInt();
+        for (int i = 1; i <=a; i++) {
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*  ");
+            }
+        System.out.println();
+        }
     }
 }
